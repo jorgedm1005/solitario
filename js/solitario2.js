de97b992e84be0cff2a485a50949faab8d9e8d7c
Your Name <you@example.com>
2023-02-24 12:50:00 +0100

    24/02/2023

--- js/solitario2.js
+++ js/solitario2.js
@@ -3,9 +3,9 @@
 // Array de palos
 let palos = ["ova", "cua", "hex", "cir"];
 // Array de número de cartas
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-//let numeros = [9, 10, 11, 12];
+let numeros = [9, 10, 11, 12];
 
 // paso (top y left) en pixeles de una carta a la siguiente en un mazo
 let paso = 5;
@@ -39,6 +39,8 @@ let cont_movimientos = document.getElementById("cont_movimientos");
 let cont_tiempo = document.getElementById("cont_tiempo"); // span cuenta tiempo
 let segundos = 0;    // cuenta de segundos
 let temporizador = null; // manejador del temporizador
+
+let tap = true; // variable para saber tapete de origen
 
 /***** FIN DECLARACIÓN DE VARIABLES GLOBALES *****/
 
@@ -76,6 +78,8 @@ function comenzar_juego() {
   barajar(mazo_inicial);  // Barajar
 
   cargar_tapete_inicial(mazo_inicial);
+
+
 
   // Dejar mazo_inicial en tapete inicial
 
@@ -122,6 +126,7 @@ function cargar_tapete_inicial(cartas) {
     carta.style.left = paso + 3 + "px";
     carta.style.width = "60px";
     carta.draggable = false;
+    carta.setAttribute("ondragstart", "dragStart(event)")
     tapete_inicial.appendChild(carta);
     paso += 5;
     console.log(carta);
@@ -133,6 +138,17 @@ function cargar_tapete_inicial(cartas) {
 
   }
 } // cargar_tapete_inicial
+
+function cargar_mazo(mazo) {
+  // cargar el mazo sobrantes en el tapete inicial
+  for (let i = 0; i < mazo.length; i++) {
+
+    let carta = document.firstChild(mazo[i]);
+    console.log(carta)
+    tapete_inicial.appendChild(carta);
+  }
+}
+
 
 
 function barajar(mazo) {
@@ -184,13 +200,14 @@ function set_contador(contador, valor) {
 
 } // set_contador
 
-function actualizar_baraja() {
+function actualizar_inicial() {
   /*** !!!!!!!!!!!!!!!!!!! CÓDIGO !!!!!!!!!!!!!!!!!!!! **/
   //actualizar baraja
   let cartas = document.querySelectorAll("#inicial img");
   for (let i = 0; i < cartas.length; i++) {
     if (i + 1 == cartas.length) {
       cartas[i].setAttribute('draggable', 'true')
+      cartas[i].setAttribute('id', 'ultima')
     } else {
       cartas[i].setAttribute('draggable', 'false')
     }
@@ -199,4 +216,243 @@ function actualizar_baraja() {
   console.log(cartas)
 } // actualizar_baraja
 
-actualizar_baraja();
+function actualizar_sobrante() {
+  /*** !!!!!!!!!!!!!!!!!!! CÓDIGO !!!!!!!!!!!!!!!!!!!! **/
+  //actualizar baraja
+  let cartas = document.querySelectorAll("#sobrantes img");
+  for (let i = 0; i < cartas.length; i++) {
+    if (i + 1 == cartas.length) {
+      cartas[i].setAttribute('draggable', 'true')
+      cartas[i].setAttribute('id', 'ultimaSobrante')
+    } else {
+      cartas[i].setAttribute('draggable', 'false')
+      cartas[i].setAttribute('id', 'sobrante')
+    }
+
+  }
+  console.log(cartas)
+} // actualizar_baraja
+
+actualizar_inicial();
+actualizar_sobrante();
+
+//funcion para declarar el inicio del drag
+
+function dragStart(event) {
+
+  event.dataTransfer.setData("text", event.target.id);
+  if (event.target.parentNode.id == "inicial") {
+    tap = true;
+  } else {
+    tap = false;
+  }
+}
+
+//let p = ["ova", "cua", "hex", "cir"];
+
+function allowDrop(event) {
+
+  // muestra por consola el atributo numero de la carta que se esta arrastrando
+
+
+  if (event.target.id == "receptor1" && mazo_receptor1.length == 0 && tap) {//comprobamos si el tapete esta vacio y si viene del inicial
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == 12) { //comprobamos si la carta es un rey
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor2" && mazo_receptor2.length == 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor3" && mazo_receptor3.length == 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor4" && mazo_receptor4.length == 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor1" && mazo_receptor1.length > 0 && tap) {//comprobamos si el tapete no esta vacio y si viene del inicial
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == mazo_receptor1[mazo_receptor1.length - 1].split("-")[0] - 1) { //comprobamos si la carta es menor q la del tapete
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor2" && mazo_receptor2.length > 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == mazo_receptor2[mazo_receptor2.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor3" && mazo_receptor3.length > 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == mazo_receptor3[mazo_receptor3.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor4" && mazo_receptor4.length > 0 && tap) {
+    if (mazo_inicial[mazo_inicial.length - 1].split("-")[0] == mazo_receptor4[mazo_receptor4.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor1" && mazo_receptor1.length == 0 && !tap) {//comprobamos si el tapete esta vacio y si viene del sobrantes
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == 12) { //comprobamos si la carta es un rey
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor2" && mazo_receptor2.length == 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor3" && mazo_receptor3.length == 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor4" && mazo_receptor4.length == 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == 12) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor1" && mazo_receptor1.length > 0 && !tap) {//comprobamos si el tapete no esta vacio y si viene del sobrantes
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == mazo_receptor1[mazo_receptor1.length - 1].split("-")[0] - 1) { //comprobamos si la carta es menor q la del tapete
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor2" && mazo_receptor2.length > 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == mazo_receptor2[mazo_receptor2.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor3" && mazo_receptor3.length > 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == mazo_receptor3[mazo_receptor3.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "receptor4" && mazo_receptor4.length > 0 && !tap) {
+    if (mazo_sobrantes[mazo_sobrantes.length - 1].split("-")[0] == mazo_receptor4[mazo_receptor4.length - 1].split("-")[0] - 1) {
+      event.preventDefault();
+
+    }
+  } else if (event.target.id == "sobrantes") { //comprobamos si va a sobrantes
+    event.preventDefault();
+  }
+  actualizar_inicial();
+  actualizar_sobrante();
+
+  console.log(mazo_inicial[mazo_inicial.length - 1].split("-")[0]);
+
+}
+
+function drop(event) {
+  event.preventDefault();
+
+  var data = event.dataTransfer.getData("text");
+
+  if (event.target.id == "receptor1" && tap) { //conreolo el tapete y si viene del inicial
+
+    mazo_receptor1.push(mazo_inicial[mazo_inicial.length - 1]);
+    mazo_inicial.pop();
+
+    tapete_receptor1.appendChild(document.getElementById(data));
+    tapete_receptor1.lastChild.style.top = "20%";
+    tapete_receptor1.lastChild.style.left = "20%";
+    actualizar_inicial();
+  } else if (event.target.id == "receptor2" && tap) {
+
+    mazo_receptor2.push(mazo_inicial[mazo_inicial.length - 1]);
+    mazo_inicial.pop();
+
+    tapete_receptor2.appendChild(document.getElementById(data));
+    tapete_receptor2.lastChild.style.top = "20%";
+    tapete_receptor2.lastChild.style.left = "20%";
+    actualizar_inicial();
+  } else if (event.target.id == "receptor3" && tap) {
+
+    mazo_receptor3.push(mazo_inicial[mazo_inicial.length - 1]);
+    mazo_inicial.pop();
+
+    tapete_receptor3.appendChild(document.getElementById(data));
+    tapete_receptor3.lastChild.style.top = "20%";
+    tapete_receptor3.lastChild.style.left = "20%";
+    actualizar_inicial();
+  } else if (event.target.id == "receptor4" && tap) {
+
+    mazo_receptor4.push(mazo_inicial[mazo_inicial.length - 1]);
+    mazo_inicial.pop();
+
+    tapete_receptor4.appendChild(document.getElementById(data));
+    tapete_receptor4.lastChild.style.top = "20%";
+    tapete_receptor4.lastChild.style.left = "20%";
+    actualizar_inicial();
+  } else if (event.target.id == "sobrantes" && tap) {
+
+    mazo_sobrantes.push(mazo_inicial[mazo_inicial.length - 1]);
+    mazo_inicial.pop();
+
+    tapete_sobrantes.appendChild(document.getElementById(data));
+    tapete_sobrantes.lastChild.style.top = "20%";
+    tapete_sobrantes.lastChild.style.left = "20%";
+
+    actualizar_inicial();
+    actualizar_sobrante();
+
+
+  } else if (event.target.id == "receptor1" && !tap) {  //conreolo el tapete y si viene del sobrantes
+    actualizar_sobrante();
+    mazo_receptor1.push(mazo_sobrantes[mazo_sobrantes.length - 1]);
+    mazo_sobrantes.pop();
+
+    tapete_receptor1.appendChild(document.getElementById(data));
+    console.log(data);
+    tapete_receptor1.lastChild.style.top = "20%";
+    tapete_receptor1.lastChild.style.left = "20%";
+    actualizar_sobrante();
+  } else if (event.target.id == "receptor2" && !tap) {
+
+    mazo_receptor2.push(mazo_sobrantes[mazo_sobrantes.length - 1]);
+    mazo_sobrantes.pop();
+
+    tapete_receptor2.appendChild(document.getElementById(data));
+    tapete_receptor2.lastChild.style.top = "20%";
+    tapete_receptor2.lastChild.style.left = "20%";
+    actualizar_sobrante();
+  } else if (event.target.id == "receptor3" && !tap) {
+
+    mazo_receptor3.push(mazo_sobrantes[mazo_sobrantes.length - 1]);
+    mazo_sobrantes.pop();
+
+    tapete_receptor3.appendChild(document.getElementById(data));
+    tapete_receptor3.lastChild.style.top = "20%";
+    tapete_receptor3.lastChild.style.left = "20%";
+    actualizar_sobrante();
+  } else if (event.target.id == "receptor4" && !tap) {
+
+    mazo_receptor4.push(mazo_sobrantes[mazo_sobrantes.length - 1]);
+    mazo_sobrantes.pop();
+
+    tapete_receptor4.appendChild(document.getElementById(data));
+    tapete_receptor4.lastChild.style.top = "20%";
+    tapete_receptor4.lastChild.style.left = "20%";
+    actualizar_sobrante();
+  }
+
+
+
+}
+console.log(tapete_inicial.length)
+
+if (tapete_inicial.length == 1) {
+  barajar(mazo_sobrantes);
+  cargar_mazo(mazo_sobrantes);
+  vaciar_sobrantes(tapete_sobrantes);
+}
+
+function vaciar_sobrantes(tapete_sobrantes) {
+
+  while (mazo_sobrantes.length > 0) {
+    tapete_sobrantes.removeChild(tapete_sobrantes.lastChild);
+    mazo_sobrantes.pop();
+  }
+}
